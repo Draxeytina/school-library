1341087a68d498c8c1b7e49cde6622ba6ae21623
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-08-29 21:46:23 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,51 +4,19 @@ require_relative './classes/teacher'
 require_relative './classes/book'
 require_relative './classes/rental'
 require_relative './classes/classroom'
+require_relative './classes/menu'
+require_relative './classes/create_person'
 
 class Main
   include App
+  include Menu
+  include Create_person
 
   def initialize
     @persons = []
     @books = []
     @rentals = []
     @classroom = Classroom.new('Programming')
-  end
-
-  def menu
-    puts "----------------------------------------------\n"
-    puts 'WELCOME TO SCHOOL LIBRARY'
-    puts "----------------------------------------------\n\n"
-
-    puts 'Please choose an option by entering a number:'
-    puts '1 - LIST ALL BOOKS'
-    puts '2 - LIST ALL PEOPLES'
-    puts '3 - CREATE A PERSON'
-    puts '4 - CREATE A BOOK'
-    puts '5 - CREATE A BOOK RENTAL'
-    puts '6 - LIST ALL RENTALS FOR A GIVEN PERSON ID'
-    puts "7 - EXIT\n\n"
-    puts "----------------------------------------------\n\n\n"
-    option = gets.chomp
-    Integer(option)
-  end
-
-  def create_person
-    puts 'Create: '
-    puts '1 - Student'
-    puts '2 - Teacher'
-    puts '3 - Cancel'
-    puts "\n"
-
-    print 'Option: '
-    option = gets.chomp.to_i
-
-    case option
-    when 1
-      create_student
-    when 2
-      create_teacher
-    end
   end
 
   def display
