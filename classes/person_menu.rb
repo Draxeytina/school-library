42b979e3329902573c02cbb1a020d3add565abc9
worashf <uppert83@gmail.com>
2022-08-29 23:54:40 +0300

    Refactor person menu

---
+++ classes/person_menu.rb
@@ -0,0 +1,19 @@
+module PersonMenu
+  def person_menu
+    puts 'Create: '
+    puts '1 - Student'
+    puts '2 - Teacher'
+    puts '3 - Cancel'
+    puts "\n"
+
+    print 'Option: '
+    option = gets.chomp.to_i
+
+    case option
+    when 1
+      create_student
+    when 2
+      create_teacher
+    end
+  end
+end
