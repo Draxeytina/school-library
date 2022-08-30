618f43b773dc9abaa456994b659389cac41ff4d8
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-08-30 08:05:07 +0200

    Update create.rb

--- classes/create.rb
+++ classes/create.rb
@@ -8,34 +8,41 @@ module Create
     true if %w[y Y].include? response
   end
 
+  def retrieve(string)
+    label = string.capitalize
+    print "#{label}: "
+    string = gets.chomp
+    return string
+  end
+
+  def creator(array, item)
+    array.push(item)
+  end
+
   def create_student
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
+    age = retrieve('age')
+    name = retrieve('name')
     print 'Has parent permission? [Y/N]: '
     parent_permission = check_response(gets.chomp)
-    @persons.push(Student.new(@classroom, age, name, parent_permission: parent_permission))
+    new_student = Student.new(@classroom, age, name, parent_permission: parent_permission)
+    creator(@persons, new_student)
     puts "Student has been successfully created! \n\n"
   end
 
   def create_teacher
-    print 'Age: '
-    age = gets.chomp
-    print 'Name: '
-    name = gets.chomp
-    print 'Specialization: '
-    specialization = gets.chomp
-    @persons.push(Teacher.new(specialization, age, name, parent_permission: true))
+    age = retrieve('age')
+    name =retrieve('name')
+    specialization = retrieve('specialization')
+    new_teacher = Teacher.new(specialization, age, name, parent_permission: true)
+    creator(@persons, new_teacher)
     puts "Teacher has been sucessfully created! \n\n"
   end
 
   def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    @books.push(Book.new(title, author))
+    title = retrieve('title')
+    author = retrieve('author')
+    new_book = Book.new(title, author)
+    creator(@books, new_book)
     puts "Book has been sucessfully created! \n\n"
   end
 
@@ -56,7 +63,8 @@ module Create
     puts 'Select person ID'
     @persons.each_with_index { |person, index| puts "#{index}: #{person.name}" }
     rental_people_id = gets.chomp.to_i
-    @rentals.push(Rental.new(@books[rent_book_id], @persons[rental_people_id], dated))
+    new_rental = Rental.new(@books[rent_book_id], @persons[rental_people_id], dated)
+    creator(@rentals, new_rental)
     puts "Rental created successfully! \n\n"
   end
 end
