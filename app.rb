b145bbaf4aa7f463c76261d554c2253c4ff00000
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-08-31 17:31:56 +0200

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,25 +4,23 @@ require './classes/classroom'
 require './classes/person_menu'
 require './classes/create'
 require './classes/display'
-
+require './classes/storage'
+require './classes/load_data'
 
 class App
   include Menu
   include PersonMenu
   include Create
   include Display
-
+  include LoadData
 
   def initialize
     @persons = []
     @books = []
     @rentals = []
     @classroom = Classroom.new('Programming')
-
-
+    load_stored_data
   end
-
-
 
   def display_menu
     loop do
@@ -39,7 +37,22 @@ class App
         create_rental
       when 6
         list_all_rentals
-      
+      else
+        quit
+      end
     end
+  end
+
+  def load_stored_data
+    @persons = create_persons(Storage.new('persons').load_data)
+    @books = create_books(Storage.new('books').load_data)
+    @rentals = create_rentals(Storage.new('rentals').load_data, @persons, @books)
+  end
+
+  def quit
+    Storage.new('persons').save_data(@persons)
+    Storage.new('books').save_data(@books)
+    Storage.new('rentals').save_data(@rentals)
+    exit
   end
 end
