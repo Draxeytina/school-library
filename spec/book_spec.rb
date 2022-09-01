dc50a09fef68ecfbac54b230f75c854b77bdb137
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-01 05:43:07 +0100

    Add test for creating rental from Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -9,5 +9,12 @@ describe Book do
       expect(book.title).to eql('Rich Dad Poor Dad')
       expect(book.author).to eql('Robert Kiyosaki')
     end
+    it 'creates rental' do
+      book = Book.new('Rich Dad Poor Dad', 'Robert Kiyosaki')
+      person = Person.new(12, 'michael', parent_permission: false)
+      rental = book.add_rental(person, '2022-08-31')
+      expect(rental.person.id).to eql(person.id)
+      expect(rental.book.id).to eql(book.id)
+    end    
   end
 end
