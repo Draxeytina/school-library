43872546d2bc6a8e1b4714cf8862c16267e03bac
emmiiorji <emmanuelorji2013@gmail.com>
2022-09-01 12:18:37 +0100

    Modify test title in book_spec.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../classes/book'
-require_relative '../classes/person.rb'
+require_relative '../classes/person'
 
 
 describe Book do
   describe 'creates book given author and title' do
-    it 'having given author and title' do
+    it 'has given author and title' do
       book = Book.new('Rich Dad Poor Dad', 'Robert Kiyosaki')
       expect(book.title).to eql('Rich Dad Poor Dad')
       expect(book.author).to eql('Robert Kiyosaki')
