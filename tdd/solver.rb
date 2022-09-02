f3fed9980c3796fad3d6409b0e6132913de3d63a
worashf <uppert83@gmail.com>
2022-09-02 17:30:53 +0300

    Update factorial solution

--- tdd/solver.rb
+++ tdd/solver.rb
@@ -1,12 +1,14 @@
 class Solver
   def factorial(number)
-    if number < 0 then 
+
+    unless number.instance_of?(Integer) && number >= 0
       raise ArgumentError.new 'Negative values not permitted'
-    elsif number == 0 || number == 1 then
-      return 1
-    else
-      return number * factorial(number-1) if number > 0
     end
+ 
+    return 1 if number == 0 
+   
+    return number * factorial(number-1) if number > 0
+  
   end
 
   def reverse(word)
